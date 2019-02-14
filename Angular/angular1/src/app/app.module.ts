62a523cfe2e5f6a424631a2566f1f32f22df0c6f
dam1-joqix <jalonsop12@iesvjp.es>
2019-02-14 18:56:30 +0100

    Routing

--- Angular/angular1/src/app/app.module.ts
+++ Angular/angular1/src/app/app.module.ts
@@ -11,6 +11,9 @@ import { PintaEstrellasPipe } from './pipes/pinta-estrellas.pipe';
 import { EstrellasRatingComponent } from './estrellas-rating/estrellas-rating.component';
 import {CargaProductoService} from "./servicios/carga-producto.service";
 import {HttpClientModule} from "@angular/common/http";
+import { BienvenidaComponent } from './bienvenida/bienvenida.component';
+import { DetallesProductoComponent } from './detalles-producto/detalles-producto.component';
+import {RouterModule} from "@angular/router";
 
 
 @NgModule({
@@ -20,12 +23,23 @@ import {HttpClientModule} from "@angular/common/http";
     ItemProductoComponent,
     FiltroProductoPipe,
     PintaEstrellasPipe,
-    EstrellasRatingComponent
+    EstrellasRatingComponent,
+    BienvenidaComponent,
+    DetallesProductoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(
+      [
+        {path:'bienvenida',component:BienvenidaComponent},
+        {path:'productos',component:ListarProductosComponent},
+        {path:'productos/:id',component:DetallesProductoComponent},
+        {path:'',redirectTo:'/bienvenida',pathMatch:'full'},
+        {path: '**',redirectTo:'/bienvenida',pathMatch:'full'},
+      ]
+    )
   ],
   providers:[CargaProductoService],
   bootstrap: [AppComponent]
---
+++ Angular/angular1/src/app/detalles-producto/detalles-producto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'detalles-producto',
+  templateUrl: './detalles-producto.component.html',
+  styleUrls: ['./detalles-producto.component.css']
+})
+export class DetallesProductoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
